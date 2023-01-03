6fa99180f1f28f54ead4a41ee870adbddbf6e746
imskanand <the.anand3099@gmail.com>
2023-01-03 14:23:33 +0530

    Index is only For reading the table in the DB

--- index.js
+++ index.js
@@ -19,29 +19,3 @@ console.log(read_posts)
 console.log(read_error);
 
 console.log("\n-----------------Inserting posts----------------------\n")
-//^ Inserting a new post in the database
-const { data: insert_posts, insert_error } = await supabase
-  .from('posts')
-  .insert([
-    {
-      body: 'This is a second post from the Js Client',
-      likes_count: 1,
-      username: 'imskanand',
-    }
-  ])
-
-console.log(insert_posts)
-console.log(insert_error)
-console.log("\n-----------------Updating posts----------------------\n")
-//^ Inserting a new post in the database
-const { data: update_posts, update_error } = await supabase
-  .from('posts')
-  .update([
-    {
-      likes_count: 7,
-    }
-  ])
-  .eq('id', 11)
-
-console.log(update_posts)
-console.log(update_error)
